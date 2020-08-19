9ec18c950ebb910e2528b0d0a46391c907b75e2f
kansology <tarun.kansal86@gmail.com>
2020-08-19 16:46:10 -0600

    add the java object.

---
+++ src/JavaObjects.java
@@ -0,0 +1,9 @@
+public class JavaObjects {
+
+    public static void main(String[] args) {
+        ArithmeticOperations ao = new ArithmeticOperations();
+
+        int sum = ao.sum(1,2);
+        System.out.println("sum = " + sum);
+    }
+}
