defcc8aea7d1ce2bdcece9dfe49b0864ea53be86
kansology <tarun.kansal86@gmail.com>
2020-08-20 21:51:35 -0600

    add the java object class.

--- src/JavaObjects.java
+++ src/JavaObjects.java
@@ -5,5 +5,8 @@ public class JavaObjects {
 
         int sum = ao.sum(1,2);
         System.out.println("sum = " + sum);
+
+        int subtract = ao.subtract(20,5);
+        System.out.println("subtract = " + subtract);
     }
 }
